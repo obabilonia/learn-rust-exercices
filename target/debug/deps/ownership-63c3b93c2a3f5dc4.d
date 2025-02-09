7505250e6e37864fafdd0d0b7df7651779beb689
Oscar Andres Babilonia Perez <oscar-babilonia@hotmail.com>
2025-02-09 11:35:59 -0300

    add helper

---
+++ target/debug/deps/ownership-63c3b93c2a3f5dc4.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/oscar/linux/learn-rust-exercices/target/debug/deps/ownership-63c3b93c2a3f5dc4: exercises/03_ticket_v1/06_ownership/src/lib.rs
+
+/mnt/c/Users/oscar/linux/learn-rust-exercices/target/debug/deps/ownership-63c3b93c2a3f5dc4.d: exercises/03_ticket_v1/06_ownership/src/lib.rs
+
+exercises/03_ticket_v1/06_ownership/src/lib.rs:
---
+++ target/debug/deps/ownership-936ca6f01c269799.d
@@ -0,0 +1,7 @@
+/mnt/c/Users/oscar/linux/learn-rust-exercices/target/debug/deps/libownership-936ca6f01c269799.rmeta: exercises/03_ticket_v1/06_ownership/src/lib.rs
+
+/mnt/c/Users/oscar/linux/learn-rust-exercices/target/debug/deps/libownership-936ca6f01c269799.rlib: exercises/03_ticket_v1/06_ownership/src/lib.rs
+
+/mnt/c/Users/oscar/linux/learn-rust-exercices/target/debug/deps/ownership-936ca6f01c269799.d: exercises/03_ticket_v1/06_ownership/src/lib.rs
+
+exercises/03_ticket_v1/06_ownership/src/lib.rs:
