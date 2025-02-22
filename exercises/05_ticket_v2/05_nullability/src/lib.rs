6b5dfa92cf995642183d4164cd281cec955d53cd
Oscar Andres Babilonia Perez <oscar-babilonia@hotmail.com>
2025-02-22 14:32:46 -0300

    solve 5.5

--- exercises/05_ticket_v2/05_nullability/src/lib.rs
+++ exercises/05_ticket_v2/05_nullability/src/lib.rs
@@ -14,6 +14,8 @@ enum Status {
     Done,
 }
 
+
+
 impl Ticket {
     pub fn new(title: String, description: String, status: Status) -> Ticket {
         if title.is_empty() {
@@ -36,7 +38,10 @@ impl Ticket {
         }
     }
     pub fn assigned_to(&self) -> Option<&String> {
-        todo!()
+        match &self.status {
+            Status::InProgress { assigned_to } => Some(assigned_to),
+            _ => None,
+        }
     }
 }
 
