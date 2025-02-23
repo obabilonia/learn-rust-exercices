c3265f6e302e49f9ce78c8e83a0f05838b0915a4
Oscar Andres Babilonia Perez <oscar-babilonia@hotmail.com>
2025-02-23 14:20:51 -0300

    solve 5.15

--- exercises/05_ticket_v2/15_outro/src/description.rs
+++ exercises/05_ticket_v2/15_outro/src/description.rs
@@ -1,8 +1,35 @@
 // TODO: Implement `TryFrom<String>` and `TryFrom<&str>` for the `TicketDescription` type,
 //   enforcing that the description is not empty and is not longer than 500 bytes.
 //   Implement the traits required to make the tests pass too.
-
+#[derive(Debug, PartialEq, Clone)]
 pub struct TicketDescription(String);
+
+#[derive(Debug, thiserror::Error)]
+pub enum ParseDescriptionError {
+    #[error("The description cannot be empty")]
+    DescriptionCannotBeEmpty,
+    #[error("The description cannot be longer than 500 bytes")]
+    DescriptionTooLong,
+}
+
+impl TryFrom<String> for TicketDescription {
+    type Error = ParseDescriptionError;
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        if value.is_empty(){ return Err(ParseDescriptionError::DescriptionCannotBeEmpty) }
+        else if value.len() > 500 { return Err(ParseDescriptionError::DescriptionTooLong)}
+        else { return Ok(Self(value)) }
+    }
+}
+
+
+impl TryFrom<&str> for TicketDescription {
+    type Error = ParseDescriptionError;
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        if value.is_empty(){ return Err(ParseDescriptionError::DescriptionCannotBeEmpty) }
+        else if value.len() > 500 { return Err(ParseDescriptionError::DescriptionTooLong)}
+        else { return Ok(Self(value.into())) }
+    }
+}
 
 #[cfg(test)]
 mod tests {
--- exercises/05_ticket_v2/15_outro/src/title.rs
+++ exercises/05_ticket_v2/15_outro/src/title.rs
@@ -1,8 +1,38 @@
 // TODO: Implement `TryFrom<String>` and `TryFrom<&str>` for the `TicketTitle` type,
 //   enforcing that the title is not empty and is not longer than 50 characters.
 //   Implement the traits required to make the tests pass too.
-
+#[derive(Debug, PartialEq, Clone)]
 pub struct TicketTitle(String);
+
+#[derive(Debug, thiserror::Error)]
+pub enum ParseTitleError {
+    #[error("The title cannot be empty")]
+    TitleCannotBeEmpty,
+    #[error("The title cannot be longer than 50 bytes")]
+    TitleTooLong,
+}
+
+impl TryFrom<String> for TicketTitle {
+    type Error = ParseTitleError;
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        if value.is_empty(){ return Err(ParseTitleError::TitleCannotBeEmpty) }
+        else if value.len() > 50 { return Err(ParseTitleError::TitleTooLong)}
+        else { return Ok(Self(value)) }
+    }
+}
+
+
+impl TryFrom<&str> for TicketTitle {
+    type Error = ParseTitleError;
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        if value.is_empty(){ return Err(ParseTitleError::TitleCannotBeEmpty) }
+        else if value.len() > 50 { return Err(ParseTitleError::TitleTooLong)}
+        else { return Ok(Self(value.into())) }
+    }
+}
+
+
+
 
 #[cfg(test)]
 mod tests {
