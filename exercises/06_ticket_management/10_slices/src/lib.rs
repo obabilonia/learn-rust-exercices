0f61e958686fbfd0c22c6744e96811203a1263db
Oscar Andres Babilonia Perez <oscar-babilonia@hotmail.com>
2025-03-09 08:54:15 -0300

    solve 6.10

--- exercises/06_ticket_management/10_slices/src/lib.rs
+++ exercises/06_ticket_management/10_slices/src/lib.rs
@@ -1,6 +1,14 @@
 // TODO: Define a function named `sum` that takes a reference to a slice of `u32` and returns the sum of all
 //  elements in the slice.
 
+fn sum(A : &[u32]) -> u32{
+    let mut aux: u32 = 0;
+    for i in A {
+        aux = aux + i;
+    }
+    aux
+} 
+
 #[cfg(test)]
 mod tests {
     use super::*;
